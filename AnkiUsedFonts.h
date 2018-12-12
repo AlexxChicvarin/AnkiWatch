c06bfedc598891318068f9e54eaca1cb301837fc
Alexx Chichvarin <ch.alexx79@gmail.com>
2018-12-12 21:41:39 +0300

    --

--- AnkiUsedFonts.h
+++ AnkiUsedFonts.h
@@ -4,12 +4,6 @@
 //#include <stdint.h>
 //#include "u8g2.h"
 
-// define????????
-//const uint8_t* font_currTime      = u8g2_font_5x7_tf;
-//const uint8_t* font_cardWord      = u8g2_font_9x15_tf;
-//const uint8_t* font_cardContent   = u8g2_font_8x13_tf;
-//const uint8_t* font_mainMenuItems = u8g2_font_9x15_tf; // main??????
-//const uint8_t* font_header        = u8g2_font_5x7_tf;  // for menu dir tree. maybe font_currTime is legasy
 
 // DEFINE!
 #define font_currTime       u8g2_font_5x7_tf
