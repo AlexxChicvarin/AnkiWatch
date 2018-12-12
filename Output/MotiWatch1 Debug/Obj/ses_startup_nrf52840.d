223651193516692eeddf503e2dac190291f2b6e9
AlexxChicvarin <ch.alexx79@gmail.com>
2018-12-12 21:52:26 +0300

    Remove output dir

--- Output/MotiWatch1 Debug/Obj/ses_startup_nrf52840.d
+++
@@ -1,2 +0,0 @@
-Output/MotiWatch1\ Debug/Obj/ses_startup_nrf52840.o: \
- D:\nRF5_SDK_15.2.0_9412b96\modules\nrfx\mdk\ses_startup_nrf52840.s
--- Output/MotiWatch1 Debug/Obj/thumb_crt0.d
+++
@@ -1,2 +0,0 @@
-Output/MotiWatch1\ Debug/Obj/thumb_crt0.o: \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 3.50/source/thumb_crt0.s
